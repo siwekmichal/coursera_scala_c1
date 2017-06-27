1f956b458e3111809e726d2bcb5ddababd5da643
msiwek <msiwek@PSL234.polsatc>
2017-06-27 20:33:31 +0200

    remove a problem with intset name.

--- progfun/src/week3/intsets.scala
+++ progfun/src/week3/intsets.scala
@@ -1,18 +1,11 @@
 package week3
 
-object intsets {
-  val t1 = new NonEmpty(3, new Empty, new Empty)          //> t1  : week3.NonEmpty = {.3.}
-  val t2 = t1 incl 4                              //> t2  : week3.IntSet = {.3{.4.}}
-  t2 incl 1                                       //> res0: week3.IntSet = {{.1.}3{.4.}}
-  t1 incl 9                                       //> res1: week3.IntSet = {.3{.9.}}
-  t2 union t1                                     //> res2: week3.IntSet = {.3{.4.}}
-  
-}
 
-abstract class IntSet {
-  def incl(x: Int): IntSet
+
+abstract class IntSet2 {
+  def incl(x: Int): IntSet2
   def contains(x: Int): Boolean
-  def union (other: IntSet): IntSet
+  def union (other: IntSet2): IntSet2
 }
 
 //object Empty extends IntSet {
@@ -21,26 +14,35 @@ abstract class IntSet {
 //  def union (other: IntSet): IntSet = other
 //  override def toString = "."
 //}
-class Empty extends IntSet {
+class Empty extends IntSet2 {
   def contains(x: Int): Boolean = false
-  def incl(x: Int): IntSet = new NonEmpty(x, new Empty, new Empty)
-  def union (other: IntSet): IntSet = other
+  def incl(x: Int): IntSet2 = new NonEmpty(x, new Empty, new Empty)
+  def union (other: IntSet2): IntSet2 = other
   override def toString = "."
 }
 
-class NonEmpty(elem: Int, left: IntSet, right: IntSet) extends IntSet {
+class NonEmpty(elem: Int, left: IntSet2, right: IntSet2) extends IntSet2 {
   def contains(x: Int): Boolean =
     if (x < elem) left contains x
     else if (x > elem) right contains x
     else true
 
-  def incl(x: Int): IntSet =
+  def incl(x: Int): IntSet2 =
     if (x < elem) new NonEmpty(elem, left incl x, right)
     else if (x > elem) new NonEmpty(elem, left, right incl x)
     else this
     
-  def union (other: IntSet): IntSet =
+  def union (other: IntSet2): IntSet2 =
   	((left union right) union other) incl elem
 
   override def toString = "{" + left + elem + right + "}"
+}
+
+object intsetstest {
+  val t1 = new NonEmpty(3, new Empty, new Empty)          //> t1  : week3.NonEmpty = {.3.}
+  val t2 = t1 incl 4                              //> t2  : week3.IntSet = {.3{.4.}}
+  t2 incl 1                                       //> res0: week3.IntSet = {{.1.}3{.4.}}
+  t1 incl 9                                       //> res1: week3.IntSet = {.3{.9.}}
+  t2 union t1                                     //> res2: week3.IntSet = {.3{.4.}}
+  
 }
